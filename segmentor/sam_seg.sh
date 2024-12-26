abf49b89714648d0f9e9c4648dd7b1e611f5c332
Eason Zhang <preachersheng@sina.com>
2024-12-26 12:09:14 +0800

    Update sam_seg.sh

--- segmentor/sam_seg.sh
+++ segmentor/sam_seg.sh
@@ -15,4 +15,4 @@ img_path=/opt/data/private/SA2M/hydra-mesa/SAM/demo/src/assets/data/dogs.jpg
 save_folder=/opt/data/private/SA2M/hydra-mesa/segmentor/seg_res
 save_name=seg_res_dogs.jpg
 
-CUDA_VISIBLE_DEVICES=$cuda_id python ImgSAMSeg.py --img_path $img_path --save_folder $save_folder --save_name $save_name
+CUDA_VISIBLE_DEVICES=$cuda_id python ImgSAMSeg.py --img_path $img_path --save_folder $save_folder --save_name $save_name --sam_name SAM
